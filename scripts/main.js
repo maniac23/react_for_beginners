d79b27e7ce8f86ee2fc8a1c4875a3ad188d295ab
Mikhail Tukai <tukaym@gmail.com>
2017-01-11 21:27:03 +0300

    added StorePicker component

---
+++ scripts/main.js
@@ -0,0 +1,16 @@
+var React = require('react');
+var ReactDOM = require('react-dom');
+
+
+var StorePicker = React.createClass({
+
+  render: function() {
+    return (
+      <p>Hello</p>
+    );
+  }
+
+});
+
+
+ReactDOM.render(<StorePicker />, document.querySelector('#main'));
