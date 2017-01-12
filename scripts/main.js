96e22b119e4e116ffdafae2cdc8ace0264aa8fe5
Mikhail Tukai <tukaym@gmail.com>
2017-01-12 23:20:21 +0300

    connected helpers

--- scripts/main.js
+++ scripts/main.js
@@ -5,6 +5,7 @@ var Router = ReactRouter.Router;
 var Route = ReactRouter.Route;
 var Navigation = ReactRouter.Navigation;
 var createBrowserHistory = require('history/lib/createBrowserHistory');
+var h = require('./helpers');
 
 var App = React.createClass({
   render: function() {
@@ -59,7 +60,7 @@ var StorePicker = React.createClass({
     return (
       <form className="store-selector">
         <h2>Please Enter a Store</h2> 
-        <input type="text" ref="storeId" required/>
+        <input type="text" ref="storeId" defaultValue={h.getFunName()} required/>
         <input type="submit" />
       </form>
     );
