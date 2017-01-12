2e704e405fedbd8dc7781fab9001cb5756e43eee
Mikhail Tukai <tukaym@gmail.com>
2017-01-12 22:14:56 +0300

    added App, Header, Order, Invantory components

--- scripts/main.js
+++ scripts/main.js
@@ -1,6 +1,44 @@
 var React = require('react');
 var ReactDOM = require('react-dom');
 
+var App = React.createClass({
+  render: function() {
+    return (
+      <div className="catch-of-the-day">
+        <div className="menu">
+          <Header />
+        </div>
+        <Order />
+        <Inventory />
+      </div>    
+    )
+  }
+});
+
+
+var Header = React.createClass({
+  render: function() {
+    return (
+      <p>Header</p>
+    )
+  }
+});
+
+var Order = React.createClass({
+  render: function() {
+    return (
+      <p>Order</p>
+    )
+  }
+});
+var Inventory = React.createClass({
+  render: function() {
+    return (
+      <p>Inventory</p>
+    )
+  }
+});
+
 
 var StorePicker = React.createClass({
 
@@ -8,7 +46,7 @@ var StorePicker = React.createClass({
     return (
       <form className="store-selector">
         <h2>Please Enter a Store</h2> 
-        <input type="text" ref="storeId" />
+        <input type="text" ref="storeId" required/>
         <input type="submit" />
       </form>
     );
@@ -17,4 +55,4 @@ var StorePicker = React.createClass({
 });
 
 
-ReactDOM.render(<StorePicker />, document.querySelector('#main'));
+ReactDOM.render(<App />, document.querySelector('#main')); 
