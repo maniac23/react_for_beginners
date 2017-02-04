1f045c63d8407d44b4c1702ca0fb763791023e27
Mikhail Tukai <tukaym@gmail.com>
2017-02-04 22:10:05 +0300

    chunked AddFishForm component

---
+++ scripts/components/AddFishForm.js
@@ -0,0 +1,38 @@
+import React from 'react';
+
+var AddFishForm = React.createClass({
+  createFish: function (event) {
+    event.preventDefault();
+    // Take the dats from the form and create an object
+    var fish = {
+      name: this.refs.name.value,
+      price: this.refs.price.value,
+      status: this.refs.status.value,
+      desc: this.refs.desc.value,
+      image: this.refs.image.value
+    }
+    // Add the fish to the App State 
+    this.props.addFish(fish);
+    this.refs.fishForm.reset();
+  },
+
+  render: function () {
+    return (
+      <form className="fish-edit" ref="fishForm" onSubmit={this.createFish} >
+        <input type="text" ref="name" placeholder="Fish Name" />
+        <input type="text" ref="price" placeholder="Fish Price" />
+        <select ref="status">
+          <option value="available">Fresh!</option>
+          <option value="unavailable">Sold out!</option>
+        </select>
+        <textarea type="text" ref="desc" placeholder="Desc"></textarea>
+        <input type="text" ref="image" placeholder="Url to image" />
+        <button type="submit">+ Add item</button>
+      </form>
+    )
+  }
+});
+
+export default AddFishForm;
+
+
--- scripts/main.js
+++ scripts/main.js
@@ -19,7 +19,7 @@ var Catalyst = require('react-catalyst');
 import NotFound from './components/NotFound';
 import StorePicker from './components/StorePicker';
 import Fish from './components/Fish';
-
+import AddFishForm from './components/AddFishForm';
 
 var App = React.createClass({
   mixins: [Catalyst.LinkedStateMixin],
@@ -102,39 +102,6 @@ var App = React.createClass({
         <Order fishes={this.state.fishes} order={this.state.order} removeFromOrder={this.removeFromOrder} />
         <Inventory addFish={this.addFish} loadSamples={this.loadSamples} fishes={this.state.fishes} linkState={this.linkState} removeFish={this.removeFish} />
       </div>
-    )
-  }
-});
-
-var AddFishForm = React.createClass({
-  createFish: function (event) {
-    event.preventDefault();
-    // Take the dats from the form and create an object
-    var fish = {
-      name: this.refs.name.value,
-      price: this.refs.price.value,
-      status: this.refs.status.value,
-      desc: this.refs.desc.value,
-      image: this.refs.image.value
-    }
-    // Add the fish to the App State 
-    this.props.addFish(fish);
-    this.refs.fishForm.reset();
-  },
-
-  render: function () {
-    return (
-      <form className="fish-edit" ref="fishForm" onSubmit={this.createFish} >
-        <input type="text" ref="name" placeholder="Fish Name" />
-        <input type="text" ref="price" placeholder="Fish Price" />
-        <select ref="status">
-          <option value="available">Fresh!</option>
-          <option value="unavailable">Sold out!</option>
-        </select>
-        <textarea type="text" ref="desc" placeholder="Desc"></textarea>
-        <input type="text" ref="image" placeholder="Url to image" />
-        <button type="submit">+ Add item</button>
-      </form>
     )
   }
 });
