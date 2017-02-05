84cf4a2ef383649e02b09460e689c487b1e7225c
Mikhail Tukai <tukaym@gmail.com>
2017-02-05 21:39:49 +0300

    refactored main.js

--- scripts/main.js
+++ scripts/main.js
@@ -1,27 +1,20 @@
 import React from 'react';
 import {render } from 'react-dom';
-var ReactRouter = require('react-router');
-var Router = ReactRouter.Router;
-var Route = ReactRouter.Route;
-var Navigation = ReactRouter.Navigation;
-
-var createBrowserHistory = require('history/lib/createBrowserHistory');
+import { Router, Route } from 'react-router';
+import { createHistory } from 'history';
 
 // import components
 import NotFound from './components/NotFound';
 import StorePicker from './components/StorePicker';
-import Fish from './components/Fish';
-import AddFishForm from './components/AddFishForm';
 import App from './components/App';
 
 // routes
 var routes = (
-  <Router history={createBrowserHistory()}>
+  <Router history={createHistory()}>
     <Route path="/" component={StorePicker} />
     <Route path="store/:storeId" component={App} />
     <Route path="*" component={NotFound} />
   </Router>
 );
-
 
 render(routes, document.querySelector('#main')); 
