ba9a1a986758fc3362fc0ae0f0a1705de41fc694
Mikhail Tukai <tukaym@gmail.com>
2017-02-05 23:25:29 +0300

    refactored other components

--- scripts/components/Inventory.js
+++ scripts/components/Inventory.js
@@ -1,8 +1,11 @@
 import React from 'react';
 import AddFishForm from './AddFishForm';
+import autobind from 'autobind-decorator';
 
-var Inventory = React.createClass({
-  renderInventory: function (key) {
+
+@autobind
+class Inventory extends React.Component {
+  renderInventory(key) {
     var linkState = this.props.linkState;
     return (
       <div className="fish-edit" key={key}>
@@ -17,8 +20,8 @@ var Inventory = React.createClass({
         <button onClick={this.props.removeFish.bind(null, key)}>Remove fish</button>
       </div>
     )
-  },
-  render: function () {
+  }
+  render() {
     return (
       <div>
         <h2>Inventory</h2>
@@ -27,15 +30,15 @@ var Inventory = React.createClass({
         <button onClick={this.props.loadSamples}>Load sample fishes</button>
       </div>
     )
-  },
-
-  propTypes: {
-    addFish: React.PropTypes.func.isRequired,
-    loadSamples: React.PropTypes.func.isRequired,
-    fishes: React.PropTypes.object.isRequired,
-    linkState: React.PropTypes.func.isRequired,
-    removeFish: React.PropTypes.func.isRequired
   }
-});
+}
+
+Inventory.propTypes = {
+  addFish: React.PropTypes.func.isRequired,
+  loadSamples: React.PropTypes.func.isRequired,
+  fishes: React.PropTypes.object.isRequired,
+  linkState: React.PropTypes.func.isRequired,
+  removeFish: React.PropTypes.func.isRequired
+}
 
 export default Inventory;
